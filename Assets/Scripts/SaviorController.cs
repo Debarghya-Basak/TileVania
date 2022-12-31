1a098b9654d9322e49ece4e0fb9acb63544ace16
Debarghya-Basak <debarghyabasakofficial.2001@gmail.com>
2022-12-31 22:48:32 +0530

    Added drag

--- Assets/Scripts/SaviorController.cs
+++ Assets/Scripts/SaviorController.cs
@@ -10,12 +10,16 @@ public class SaviorController : MonoBehaviour
     [SerializeField] Sprite playerIdleSprite;
     Animator animator;
     Vector2 moveInput;
+    Vector2 moveInputVertical;
     Rigidbody2D playerBody;
 
-    float runSpeed = 2.5f;
-    float jumpForce = 4.5f;
-    bool hasJumped = false;
+    float runSpeed = 3f;
+    float jumpForce = 5f;
+    float dragOnGround = 4f;
     bool inLadder = false;
+    bool isClimbing = false;
+    bool isTouchingGround = false;
+    bool isRunning = false;
 
     void Start()
     {
@@ -28,21 +32,39 @@ public class SaviorController : MonoBehaviour
     void Update()
     {
         run();
+        climb();
+
+        if(inLadder){
+            playerBody.gravityScale = 0.3f;
+        }
+            
+        
+        if(isTouchingGround)
+            playerBody.drag = dragOnGround;
+        else 
+            playerBody.drag = 0f;
+
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if(other.collider.tag == "Platform")
-            hasJumped = false;
+        if(other.collider.tag == "Platform"){
+            Debug.Log("Touching ground");
+            isTouchingGround = true;
+        }
+        
+    }
 
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.collider.tag == "Platform"){
+            Debug.Log("Not touching ground");
+            isTouchingGround = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Ladder"){
             inLadder = true;
-            //TODO: Change sprite to ladder climb
-
-            animator.enabled = false;
-            animator.SetBool("isClimbing", true);
+            //animator.enabled = false;
             playerSprite.sprite = playerClimbSprite;
             Debug.Log("In ladder");
         }
@@ -50,10 +72,12 @@ public class SaviorController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other) {
         if(other.tag == "Ladder"){
-            animator.enabled = true;
+            //animator.enabled = true;
             animator.SetBool("isClimbing", false);
             playerSprite.sprite = playerIdleSprite;
             inLadder = false;
+            isClimbing = false;
+            playerBody.gravityScale = 0.98f;
             Debug.Log("Outside ladder");
         }
             
@@ -62,32 +86,44 @@ public class SaviorController : MonoBehaviour
     void OnMoveLeftRight(InputValue inputValue)
     {
         moveInput = inputValue.Get<Vector2>();
+
         if(moveInput != new Vector2(0f,0f)){
+
+            isRunning = true;
             animator.SetBool("isRunning", true);
+            
             if(moveInput.x<0)
                 playerSprite.flipX = true;
             else if(moveInput.x>0)
                 playerSprite.flipX = false;
+
         }
-        else    
+        else{
+
+            isRunning = false;
             animator.SetBool("isRunning", false);
+        }    
 
         Debug.Log(moveInput);
         
     }
 
     void OnJump(InputValue inputValue){
-        //playerBody.AddForce(new Vector2(1f,0f));
-        if(!hasJumped && !inLadder){
+
+        moveInputVertical = inputValue.Get<Vector2>();
+
+        if(isTouchingGround && !inLadder && moveInputVertical.y != 0f){
+
             playerBody.velocity = new Vector2(0f, jumpForce);
-            hasJumped = true;
             Debug.Log("Jump");
         }
 
-        if(inputValue.Get<Vector2>() != new Vector2(0f,0f) && inLadder){
+        if(moveInputVertical != new Vector2(0f,0f) && inLadder){
+            isClimbing = true;
             Debug.Log("Climbing inside ladder start");
         }
-        else if(inputValue.Get<Vector2>() == new Vector2(0f,0f) && inLadder){
+        else if(moveInputVertical == new Vector2(0f,0f) && inLadder){
+            isClimbing = false;
             Debug.Log("Climbing inside ladder stop");
         }
         
@@ -95,8 +131,21 @@ public class SaviorController : MonoBehaviour
 
     void run(){
 
-        if(moveInput != new Vector2(0f,0f))
+        if(moveInput.x != 0f)
             playerBody.velocity = new Vector2(moveInput.x * runSpeed, playerBody.velocity.y);
         
+    }
+
+    void climb(){
+
+        if(isClimbing){
+            playerBody.velocity = new Vector2(0f,moveInputVertical.y * runSpeed);
+            animator.enabled = true;
+            animator.SetBool("isClimbing", true);
+        }
+        // else{
+        //     animator.enabled = false;
+        // }
+
     }
 }
