7eb15f3b6df8a64f608a3d9266b5aa70b5617059
Debarghya-Basak <debarghyabasakofficial.2001@gmail.com>
2023-01-03 01:18:23 +0530

    Enemy collider box bug fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -20,6 +20,19 @@ public class GameController : MonoBehaviour
             StartCoroutine(resetGame());
     }
 
+    public void nextLevel(){
+        StartCoroutine(changeToNextLevel());
+    }
+
+    IEnumerator changeToNextLevel(){
+        yield return new WaitForSecondsRealtime(2);
+        int len = SceneManager.GetAllScenes().Length;
+        if(SceneManager.GetActiveScene().buildIndex < len-1)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            SceneManager.LoadScene(0);
+    }
+
     IEnumerator reduceLife(){
         playerLife--;
         Debug.Log("Player Life : " + playerLife);
